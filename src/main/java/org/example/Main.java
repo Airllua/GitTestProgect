f745276a531e6ed95165866d83d37bbb9cf8a531
gertlolp <danik.smor@gmail.com>
2024-01-09 20:27:03 +0300

    commit4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("dkl;rtyuio");
+        System.out.println("jjjjjjjjjj");
     }
 
     public String test() {
