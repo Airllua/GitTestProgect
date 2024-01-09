9c1bb1ac58718f67db15cb542d48054cee1a82a4
gertlolp <danik.smor@gmail.com>
2024-01-09 20:10:28 +0300

    789

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("sdstfyguhk");
         System.out.println("blabla");
+        System.out.println("789");
     }
 }
